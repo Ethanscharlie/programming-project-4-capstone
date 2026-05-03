a84cfcb0c1f51180753d299ef8a2ef26e4b6c986
Ethan J. Hadley <ethanscharlie@protonmail.com>
2026-05-03 16:04:55 -0600

    Added docs to player class

--- src/main/java/io/github/ethanscharlie/Player.java
+++ src/main/java/io/github/ethanscharlie/Player.java
@@ -10,11 +10,20 @@ public class Player {
     int getOutOfJailAttempts = 0;
     boolean inJail = false;
 
+    /**
+     * @param board a reference to a board object (should be created separately).
+     * @param jailStrategy the jail exit strategy to use.
+     */
     public Player(Board board, JailStrategy jailStrategy) {
         this.board = board;
         this.jailStrategy = jailStrategy;
     }
 
+    /**
+     * Simulates the player taking a turn.
+     * Handles moves and jails.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     public void takeTurn() throws Exception {
         var canTakeAnotherTurn = false;
 
@@ -41,22 +50,42 @@ public class Player {
         }
     }
 
+    /**
+     * Moves forwards a number of steps.
+     * Will call onSquareLanding when landing.
+     * @param steps the number of steps to move.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     public void move(int steps) throws Exception {
         for (int step = 0; step < steps; step ++) location ++;
         if (location >= board.getAmountOfSquares()) location -= board.getAmountOfSquares();
         onSquareLanding();
     }
 
+    /**
+     * Moves backwards a number of steps.
+     * Will call onSquareLanding when landing.
+     * @param steps the number of steps to move.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     public void moveBack(int steps) throws Exception {
         for (int step = 0; step < steps; step ++) location --;
         if (location < 0) location = board.getAmountOfSquares() + location;
         onSquareLanding();
     }
 
+    /**
+     * Increases the amount of "Get out of Jail Free" cards that the player is holding.
+     */
     public void addGetOutOfJailFreeCard() {
         getOutOfJailCards ++;
     }
 
+    /**
+     * Moves the player to the nearest square of a type.
+     * @param squareType the type of square to move to.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     public void moveToNearest(Square.SquareType squareType) throws Exception {
         for (int i = location; i < board.getAmountOfSquares(); i ++) {
             if (board.getSquareAtLocation(i).type == squareType)  {
@@ -78,6 +107,10 @@ public class Player {
         throw new Exception("Square with type couldn't be found. " + squareType.toString());
     }
 
+    /**
+     * Runs when a square is landed on by a move function.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     private void onSquareLanding() throws Exception {
         var square = board.getSquareAtLocation(location);
         square.landingCount ++;
@@ -89,6 +122,11 @@ public class Player {
         }
     }
 
+    /**
+     * Uses the given jail exit strategy to exit the jail after landing there.
+     * Will set inJail to false when ready to leave and will call another turn or move automatically.
+     * @throws Exception can throw if a location not on the board is tried to be accessed.
+     */
     private void attemptToLeaveJail() throws Exception {
         switch (jailStrategy) {
             case ImmediateExit -> {
@@ -129,15 +167,26 @@ public class Player {
         }
     }
 
+    /**
+     * Changes the players position to the given location on the board.
+     * @param location a location index on the board.
+     */
     public void goToLocation(int location) {
         this.location = location;
     }
 
+    /**
+     * Changes the player location to the jail.
+     */
     private void goToJail() {
         location = board.getJailLocation();
         inJail = true;
     }
 
+    /**
+     * Simulates rolling a 6 sided dice.
+     * @return the value of the roll.
+     */
     private static int rollDice() {
         return (int)(Math.random() * 6) + 1;
     }
